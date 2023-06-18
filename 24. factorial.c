e6368c8e9a79b3be039a985dbf759a1a8d0967fc
MD Ahsanul Hoque Abir <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-06-18 23:19:16 +0600

    Update 24. factorial.c

--- 24. factorial.c
+++ 24. factorial.c
@@ -4,14 +4,19 @@
 int main()
 {
     int n;
+    long long int f;
     printf("Enter number: ");
     scanf("%d",&n);
     printf("The factorial of %d is: ",n);
     for(int i=1;i<=n;i++)
     {
+        f=f*i;
         if(i<n)
             printf("%d X ",i);
         else
             printf("%d",i);
+
     }
+    printf(" = %lld",f);
 }
+
