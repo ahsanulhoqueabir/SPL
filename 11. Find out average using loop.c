b551f501eabab1751e51e79450a8159d47197ed0
MD Ahsanul Hoque Abir <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-06-18 23:13:21 +0600

    Update 11. Find out average using loop.c

--- 11. Find out average using loop.c
+++ 11. Find out average using loop.c
@@ -1,1 +1,22 @@
 // Find out the average using loop
+
+#include<stdio.h>
+int main ()
+{
+	int n, i;
+	float sum=0, num, avg;
+	printf ("Enter the set of numbers:");
+	scanf ("%d", &n);
+
+
+	for (i=1; i<=n; i++) {
+		printf ("Enter the number %d:", i);
+		scanf ("%f", &num);
+		sum= sum + num;
+
+	}
+	avg= sum / n;
+	printf ("Average= %.2f", avg);
+	return 0;
+	}
+
