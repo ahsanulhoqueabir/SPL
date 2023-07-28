b29ff15bac53412d35bd84671b291b7279b3c5c1
MD Ahsanul Hoque Abir <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-07-28 21:35:59 +0600

    Update 30. Series.c

--- 30. Series.c
+++ 30. Series.c
@@ -4,12 +4,17 @@ int main()
     int i,n,s=0;
     printf("Enter the last number of series");
     scanf("%d",&n);
-    if(n%2==0){
-        s=s-n;
-    }
-    else{
-        s=s+n;
-    }
+    for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                s=s-i;
+            }
+            else
+            {
+                s=s+i;
+            }
+        }
     printf("The Sum is: %d",s);
 }
 
