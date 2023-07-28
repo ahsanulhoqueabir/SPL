18e58577c511802c31fcda8657f8315c622eb40f
MD Ahsanul Hoque Abir <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-07-28 22:38:07 +0600

    code edited

--- 35. E - Fibonacci.c
+++ 35. E - Fibonacci.c
@@ -1,110 +1,18 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    switch(n)
-    {
-    case 1:
-        printf("1");
-    case 2:
-        printf("1");
-     case 3:
-        printf("2");
-     case 4:
-        printf("3");
-    case 5:
-        printf("5");
-     case 6:
-        printf("8");
-     case 7:
-        printf("13");
-    case 8:
-        printf("21");
-     case 9:
-        printf("34");
-     case 10:
-        printf("55");
-    case 11:
-        printf("89");
-    case 12:
-        printf("144");
-     case 13:
-        printf("233");
-     case 4:
-        printf("377");
-    case 5:
-        printf("610");
-     case 6:
-        printf("987");
-     case 7:
-        printf("1597");
-    case 8:
-        printf("2584");
-     case 9:
-        printf("4181");
-     case 10:
-        printf("55");
-    case 1:
-        printf("1");
-    case 2:
-        printf("1");
-     case 3:
-        printf("2");
-     case 4:
-        printf("3");
-    case 5:
-        printf("5");
-     case 6:
-        printf("8");
-     case 7:
-        printf("13");
-    case 8:
-        printf("21");
-     case 9:
-        printf("34");
-     case 10:
-        printf("55");
-    case 1:
-        printf("1");
-    case 2:
-        printf("1");
-     case 3:
-        printf("2");
-     case 4:
-        printf("3");
-    case 5:
-        printf("5");
-     case 6:
-        printf("8");
-     case 7:
-        printf("13");
-    case 8:
-        printf("21");
-     case 9:
-        printf("34");
-     case 10:
-        printf("55");
-    case 1:
-        printf("1");
-    case 2:
-        printf("1");
-     case 3:
-        printf("2");
-     case 4:
-        printf("3");
-    case 5:
-        printf("5");
-     case 6:
-        printf("8");
-     case 7:
-        printf("13");
-    case 8:
-        printf("21");
-     case 9:
-        printf("34");
-     case 10:
-        printf("55");
+#include <stdio.h>
+int main() {
+    int n, i;
+    long long int fib1 = 0, fib2 = 1, next;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series up to %d terms:\n", n);
+    printf("%lld, %lld, ", fib1, fib2);
+
+    for (i = 2; i < n; i++) {
+        next = fib1 + fib2;
+        printf("%lld, ", next);
+        fib1 = fib2;
+        fib2 = next;
     }
-return 0;
+    printf("\n");
+    return 0;
 }
